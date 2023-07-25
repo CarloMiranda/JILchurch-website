d50a445803731f2ff1bc22110aeb9f9ed8725d0e
Mario Carlo Miranda <carlomiranda161@gmail.com>
2023-07-26 01:27:52 +0800

    added content in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,53 @@
+import React from 'react';
 
 function Home() {
 
     return(
-        <></>
+    <div className='home'>
+       <div className="banner">
+            <div className="banner-image w-100 vh-100"></div>
+       </div>
+
+        <div className="our-text row text-center justify-content-center text-white">
+            <div className="col-md-3">
+                <h1><strong>OUR MISSION</strong></h1>
+                <p>To bring all peoples to the kingdom of the living God regardless of race, status, belief and religious affiliations through the saving, healing, delivering, and transforming power of the Lord Jesus Christ.</p>
+            </div>
+            <div className="col-md-3">
+                <h1><strong>OUR VISION</strong></h1>
+                <p>A glorious church evangelizing and discipling Filipinos and all the peoples of the world through teaching, preaching and living-out the full-Gospel of the Lord Jesus Christ.</p>
+            </div>
+            <div className="col-md-3">
+                <h1><strong>OUR CORE VALUES</strong></h1>
+                <p>Passionate Love for God Love and Compassion for Others Integrity Faithfulness Excellence</p>
+            </div>
+        </div>
+
+        <div className="parallax w-100 vh-100">
+            <div className="parallax-hero vh-100">
+                <div className="home-content fs-5 text-white">
+                    <h2><strong>THE MAN, HIS MESSAGE AND THE MINISTRY</strong></h2>
+                    <p>From a communist-atheist to one of the most powerful preachers of God’s Word in the world today… From a radical activist to the founder-pastor of one of the largest Christ-centered, Bible-based, full-gospel churches in the world… From a battle-scarred street parliamentarian to one of the most respected Christian leaders in the Philippines whose spiritual counsel is sought after by high-ranking officials and distinguished leaders of the nation… From a persecuted “small, country preacher’ to a world-renown radio-TV evangelist, prophet and apostle whose vision and mission is to see the nations of the world evangelized and transformed by the same Gospel that changed his life.
+                    This in a capsule captures the exciting life story of Eddie Villanueva,
+                    fondly called Bro. Eddie or Bishop Eddie.</p>
+                    <a href="" className="btn rounded border border-white text-white">Learn More</a>
+                </div>
+            </div>
+        </div>
+        
+        <h1 className="text-center"><strong>EXAMPLE CONTENT</strong></h1>
+        <div className="home-content1">
+            
+            <h3><strong>THE MAN, HIS MESSAGE AND THE MINISTRY</strong></h3>
+            <p>From a communist-atheist to one of the most powerful preachers of God’s Word in the world today… From a radical activist to the founder-pastor of one of the largest Christ-centered, Bible-based, full-gospel churches in the world… From a battle-scarred street parliamentarian to one of the most respected Christian leaders in the Philippines whose spiritual counsel is sought after by high-ranking officials and distinguished leaders of the nation… From a persecuted “small, country preacher’ to a world-renown radio-TV evangelist, prophet and apostle whose vision and mission is to see the nations of the world evangelized and transformed by the same Gospel that changed his life.
+
+            This in a capsule captures the exciting life story of Eddie Villanueva,
+
+            fondly called Bro. Eddie or Bishop Eddie.</p>
+        </div>
+        <div className="parallax w-100 vh-100"></div>
+    </div>
+        
     )
 }
 
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,123 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../css/Index.css';
 import { Outlet, Link } from 'react-router-dom';
+import logo from '../images/logo.png';
+import ScrollToTopButton from "./ScrollToTopButton";
 import { FaInstagram, FaTwitter, FaFacebook, FaYoutube, FaPhone, FaMapMarker,FaEnvelope } from "react-icons/fa";
 
 function Layout() {
 
     return(
         <div className="layout">
-            <div className="container-fluid">
-                <div className="navbar navbar-expand-lg">
-                        <Link to="/" className='navbar-brand col-2'>Home Logo</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="nav w-100 justify-content-center">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    BULLETINS
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Articles</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li><a className="dropdown-item" href="#">Resources</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    MINISTRY
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">WAN</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li><a className="dropdown-item" href="#">SOS</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li><a className="dropdown-item" href="#">Lifegroup</a></li>
-                                </ul>
-                            </li>
-                            {/* <li className="nav-item">
-                                <Link to="/ministry" className='nav-link'>MINISTRY</Link>
-                            </li> */}
-                            <li className="nav-item">
-                                <Link to="/outreach" className='nav-link'>OUTREACH</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/network" className='nav-link'>NETWORK</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/church" className='nav-link'>CHURCH</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/contact" className='nav-link'>CONTACT US</Link>
-                            </li>
-                        </ul>
-                        <div className='social-icons col-2'>
-                            <a href="" className="social-link mx-3"><FaFacebook/></a>
-                            <a href="" className="social-link"><FaInstagram/></a>
-                            <a href="" className="social-link mx-3"><FaTwitter/></a>
-                            <a href="" className="social-link"><FaYoutube/></a>
+            <div className="navbar navbar-expand-lg">
+                    <Link to="/" className='navbar-brand col-1 justify-content-end d-flex ms-5'><img src={logo} alt="Church logo" /></Link>
+                    <button className="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="nav w-100 justify-content-center fs-5 fw-bold">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                BULLETINS
+                            </a>
+                            <ul className="dropdown-menu fs-4">
+                                <li><a className="dropdown-item top" href="#">Articles</a></li>
+                                <li><a className="dropdown-item" href="#">Resources</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                MINISTRIES
+                            </a>
+                            <ul className="dropdown-menu fs-4">
+                                <li><a className="dropdown-item top" href="#">WAN</a></li>
+                                <li><a className="dropdown-item" href="#">SOS</a></li>
+                                <li><a className="dropdown-item" href="#">Lifegroup</a></li>
+                            </ul>
+                        </li>
+                        {/* <li className="nav-item">
+                            <Link to="/ministry" className='nav-link'>MINISTRY</Link>
+                        </li> */}
+                        <li className="nav-item">
+                            <Link to="/outreach" className='nav-link text-white'>OUTREACH</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/network" className='nav-link text-white'>NETWORK</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/church" className='nav-link text-white'>CHURCH</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact" className='nav-link text-white'>CONTACT US</Link>
+                        </li>
+                    </ul>
+                    <div className='social-icons col-2 fs-4'>
+                        <a href="" className="social-link text-white mx-3"><FaFacebook/></a>
+                        <a href="" className="social-link text-white"><FaInstagram/></a>
+                        <a href="" className="social-link text-white mx-3"><FaTwitter/></a>
+                        <a href="" className="social-link text-white"><FaYoutube/></a>
+                    </div>
+                </div>
+            </div>
+
+            < Outlet />
+
+            <footer className='text-white'>
+                <div className="text-center mb-5">
+                    <h3><strong>CONNECT WITH US</strong></h3>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 d-flex text-center justify-content-center">
+                        <div className='mx-3'>
+                            <div>
+                                <span className='rounded p-2 pt-1 bg-white text-primary'><FaPhone/></span>
+                            </div>
+                            <p className='mt-3'><strong>Phone Hotline</strong></p>
+                        </div>
+                        <div className='mx-5'>
+                            <div>
+                                <span className='rounded p-2 pt-1 bg-white text-primary'><FaMapMarker/></span>
+                            </div>
+                            <p className='mt-3'><strong>Address</strong></p>
+                        </div>
+                        <div className='mx-5'>
+                            <div>
+                                <span className='rounded p-2 pt-1 bg-white text-primary'><FaEnvelope/></span>
+                            </div>
+                            <p className='mt-3'><strong>Email</strong></p>
+                        </div>
+                    </div>
+                    <div className="footer-socials col-md-6 d-flex justify-content-center">
+                        <div className='mx-3'>
+                            <span className='rounded p-2 pt-1 bg-white text-primary'><FaFacebook/></span>
+                        </div>
+                        <div className='mx-3'>
+                            <span className='rounded p-2 pt-1 bg-white text-primary'><FaInstagram/></span>
+                        </div>
+                        <div className='mx-3'>
+                            <span className='rounded p-2 pt-1 bg-white text-primary'><FaTwitter/></span>
+                        </div>
+                        <div className='mx-3'>
+                            <span className='rounded p-2 pt-1 bg-white text-primary'><FaYoutube/></span>
                         </div>
                     </div>
                 </div>
-
-                < Outlet />
-
-                <footer>
-                    <div className="text-center mb-5">
-                        <h3><strong>CONNECT WITH US</strong></h3>
-                    </div>
-                    <div className="footer-content d-flex justify-content-around">
-                        <div className="footer-contact d-flex text-center">
-                            <div className='mx-3'>
-                                <div>
-                                    <span className='rounded p-2 pt-1 bg-primary text-white'><FaPhone/></span>
-                                </div>
-                                <p className='mt-3'><strong>Phone Hotline</strong></p>
-                            </div>
-                            <div className='mx-5'>
-                                <div>
-                                    <span className='rounded p-2 pt-1 bg-primary text-white'><FaMapMarker/></span>
-                                </div>
-                                <p className='mt-3'><strong>Address</strong></p>
-                            </div>
-                            <div className='mx-5'>
-                                <div>
-                                    <span className='rounded p-2 pt-1 bg-primary text-white'><FaEnvelope/></span>
-                                </div>
-                                <p className='mt-3'><strong>Email</strong></p>
-                            </div>
-                        </div>
-                        <div className="footer-socials d-flex text-center">
-                            <div className='mx-3'>
-                                <div>
-                                    <span className='rounded p-2 pt-1 bg-primary text-white'><FaFacebook/></span>
-                                </div>
-                                <p className='mt-3'><strong>UserName</strong></p>
-                            </div>
-                            <div className='mx-3'>
-                                <div>
-                                    <span className='rounded p-2 pt-1 bg-primary text-white'><FaInstagram/></span>
-                                </div>
-                                <p className='mt-3'><strong>UserName</strong></p>
-                            </div>
-                            <div className='mx-3'>
-                                <div>
-                                    <span className='rounded p-2 pt-1 bg-primary text-white'><FaTwitter/></span>
-                                </div>
-                                <p className='mt-3'><strong>UserName</strong></p>
-                            </div>
-                            <div className='mx-3'>
-                                <div>
-                                    <span className='rounded p-2 pt-1 bg-primary text-white'><FaYoutube/></span>
-                                </div>
-                                <p className='mt-3'><strong>UserName</strong></p>
-                            </div>
-                        </div>
-                    </div>
-                    <p className='text-center'>Privacy Policy | Copyright © 2023 Jesus Is Lord Church. All rights Reserved</p>
-                </footer>
-            </div>
+                <p className='text-center'>Privacy Policy | Copyright © 2023 Jesus Is Lord Church. All rights Reserved</p>
+                <ScrollToTopButton />
+            </footer>
         </div>
     )
 }
